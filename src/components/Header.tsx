4eb030030bc9ed985d13a8fc32a66f3db3126ba8
Jana Jolovic <janajolovic04@gmail.com>
2022-03-12 23:42:06 +0100

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png" alt="" />
+        <h1>Pokemon</h1>
+    </div>
+  )
+}
+
+export default Header
