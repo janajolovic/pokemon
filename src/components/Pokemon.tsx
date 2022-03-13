be276405fbdd6ac809629553a5139fcb94934a74
Jana Jolovic <janajolovic04@gmail.com>
2022-03-13 15:37:43 +0100

    initial

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -3,24 +3,31 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
 const Pokemon = () => {
-  const [pokemon, setPokemon] = useState([])
+  const [pokemon, setPokemon] = useState<any>({})
 
-  const pokemon_name = useParams()
+  const pokemon_name = useParams().name;
   const getPokemon = async () => {
     try { 
-      let res: any = await axios.get("")
-      console.log(res.data)
+      let res: any = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`)
       setPokemon(res.data)
     } catch (err) {
       console.log(err)
     }
   }
-
   useEffect(() => {
     getPokemon()
-  }, [])
+}, [])
+
   return (
-    <div>Pokemon</div>
+    <div className='pokemonCard'>
+        {/* <img src={pokemon.sprites.other.dream_world.front_default} alt="" /> */}
+        <div>
+            {console.log(pokemon.sprites?.other)}
+            <h1>{pokemon.name}</h1>
+            <h3>Height: {pokemon.height}</h3>
+            <h3>Weight: {pokemon.weight}</h3>
+        </div>
+    </div>
   )
 }
 
